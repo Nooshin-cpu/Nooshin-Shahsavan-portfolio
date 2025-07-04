7116ee8c03057b7d960a272fbe1cabe9167ac2f5
Nooshin-cpu <sn.shahsavan@gmail.com>
2025-07-03 17:04:20 -0700

    fix capitalization

--- src/components/deli/StoryOne.tsx
+++ src/components/deli/StoryOne.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import video from "../../assets/deli/Story-1-deli.mp4";
+import video from "../../assets/deli/story-1-deli.mp4";
 
 const Section = styled.section`
   width: 100vw;
