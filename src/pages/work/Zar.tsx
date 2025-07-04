56a54827b99a816606fdf012d072da69eac9eab9
Nooshin-cpu <sn.shahsavan@gmail.com>
2025-07-04 14:25:01 -0700

    fix-floating/zar

--- src/pages/work/Zar.tsx
+++ src/pages/work/Zar.tsx
@@ -8,7 +8,6 @@ import ZarPackaging from "../../components/zar/ZarPackaging";
 import Zarmock1 from "../../components/zar/Zarmock1";
 import Zarmock2 from "../../components/zar/Zarmock2";
 import Zarmock3 from "../../components/zar/Zarmock3";
-
 import Zarmock4 from "../../components/zar/Zarmock4";
 import ZarMock5 from "../../components/zar/ZarMock5";
 import ZarStory from "../../components/zar/ZarStory";
@@ -17,7 +16,6 @@ import ZarMock7 from "../../components/zar/Zarmock7";
 import ZarMock8 from "../../components/zar/Zarmock8";
 import ZarLogoP from "../../components/zar/ZarLogoP";
 import ProblemZar from "../../components/zar/ProblemZar";
-
 import MenuWork from '../../components/MenuWork';
 import '../../components/MenuWork.css';
 import styled, { createGlobalStyle } from 'styled-components';
@@ -28,11 +26,23 @@ const PageBg = styled.div`
   background: #fff;
   transition: background 0.2s;
   overflow-x: hidden;
+  box-sizing: border-box;
+
+  @media (max-width: 600px) {
+    padding-left: 8px;
+    padding-right: 8px;
+    width: 100%;
+  }
 `;
 
 const GlobalStyle = createGlobalStyle`
   body {
     background: #fff;
+    overflow-x: hidden !important;
+    margin: 0;
+    padding: 0;
+  }
+  html {
     overflow-x: hidden;
   }
 `;
@@ -45,7 +55,6 @@ const Zar: React.FC = () => (
     <ProblemZar />
     <ZarThird/>
     <ZarLogoP />
-
     <ZarColor/>
     <ZarType/>
     <ZarPackaging/>
@@ -58,7 +67,6 @@ const Zar: React.FC = () => (
     <ZarMock6/>
     <ZarMock7/>
     <ZarMock8/>
-    
     <MenuWork />
   </PageBg>
 );
